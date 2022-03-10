cc496eb99f9720b1535bda39b99ebd4484e7a001
Erdem Alpay <erdemalpay@gmail.com>
2022-03-10 10:08:43 +0300

    Make location endpoint public

--- src/modules/location/location.controller.ts
+++ src/modules/location/location.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Public } from '../auth/public.decorator';
 import { LocationService } from './location.service';
-import { CreateLocationDto } from './dto/create-location.dto';
 
 @Controller('location')
 export class LocationController {
   constructor(private readonly locationService: LocationService) {}
 
   @Get()
+  @Public()
   findAll() {
     return this.locationService.findAll();
   }
