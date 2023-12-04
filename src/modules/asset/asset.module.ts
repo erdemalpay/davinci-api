3fe266e0ea6a6470780ec5c2c83e83531ce07123
Erdem Alpay <erdemalpay@gmail.com>
2023-12-04 10:19:40 +0300

    Update staging script

--- src/modules/asset/asset.module.ts
+++ src/modules/asset/asset.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MenuModule } from '../menu/menu.module';
 import { AssetController } from './asset.controller';
 import { AssetService } from './asset.service';
 
 @Module({
-  imports: [MenuModule],
   providers: [AssetService],
   exports: [AssetService],
   controllers: [AssetController],
