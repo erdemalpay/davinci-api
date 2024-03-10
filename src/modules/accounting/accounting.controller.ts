bcf5a55d61e603a66659613826e1d3a4692d192e
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-03-10 18:56:58 -0500

    product and unit controllers are set

--- src/modules/accounting/accounting.controller.ts
+++ src/modules/accounting/accounting.controller.ts
@@ -1,8 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UpdateQuery } from 'mongoose';
+import { CreateProductDto, CreateUnitDto } from './accounting.dto';
+import { Product } from './product.schema';
 
 import { AccountingService } from './accounting.service';
+import { Unit } from './unit.schema';
 
 @Controller('/accounting')
 export class AccountingController {
   constructor(private readonly accountingService: AccountingService) {}
+
+  // Products
+  @Get('/products')
+  getCategories() {
+    return this.accountingService.findAllProducts();
+  }
+
+  @Post('/products')
+  createCategory(@Body() createProductDto: CreateProductDto) {
+    return this.accountingService.createProduct(createProductDto);
+  }
+
+  @Patch('/products/:id')
+  updateCategory(
+    @Param('id') id: number,
+    @Body() updates: UpdateQuery<Product>,
+  ) {
+    return this.accountingService.updateProduct(id, updates);
+  }
+
+  @Delete('/products/:id')
+  deleteCategory(@Param('id') id: number) {
+    return this.accountingService.removeProduct(id);
+  }
+  // Units
+  @Get('/units')
+  getUnits() {
+    return this.accountingService.findAllUnits();
+  }
+
+  @Post('/units')
+  createUnit(@Body() createUnitDto: CreateUnitDto) {
+    return this.accountingService.createUnit(createUnitDto);
+  }
+
+  @Patch('/units/:id')
+  updateUnit(@Param('id') id: number, @Body() updates: UpdateQuery<Unit>) {
+    return this.accountingService.updateUnit(id, updates);
+  }
+
+  @Delete('/units/:id')
+  deleteUnit(@Param('id') id: number) {
+    return this.accountingService.removeUnit(id);
+  }
 }
