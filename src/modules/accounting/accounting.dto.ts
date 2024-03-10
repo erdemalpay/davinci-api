3e167f85d114ec7e182e417cf46bf0970970fc02
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-03-10 18:50:14 -0500

    Product and unit services are set

---
+++ src/modules/accounting/accounting.dto.ts
@@ -0,0 +1,8 @@
+export class CreateProductDto {
+  name: string;
+  unit: number;
+}
+
+export class CreateUnitDto {
+  name: string;
+}
--- src/modules/accounting/accounting.service.ts
+++ src/modules/accounting/accounting.service.ts
@@ -1,5 +1,6 @@
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, UpdateQuery } from 'mongoose';
+import { CreateProductDto, CreateUnitDto } from './accounting.dto';
 import { Product } from './product.schema';
 import { Unit } from './unit.schema';
 
@@ -7,6 +8,36 @@ export class AccountingService {
   constructor(
     @InjectModel(Product.name)
     private productModel: Model<Product>,
-    @InjectModel(Unit.name) private itemModel: Model<Unit>,
+    @InjectModel(Unit.name) private unitModel: Model<Unit>,
   ) {}
+  //   Products
+  findAllProducts() {
+    return this.productModel.find().sort({ order: 'asc' });
+  }
+  createProduct(createProductDto: CreateProductDto) {
+    return this.productModel.create(createProductDto);
+  }
+  updateProduct(id: number, updates: UpdateQuery<Product>) {
+    return this.productModel.findByIdAndUpdate(id, updates, {
+      new: true,
+    });
+  }
+  removeProduct(id: number) {
+    return this.productModel.findByIdAndRemove(id);
+  }
+  //   Units
+  findAllUnits() {
+    return this.unitModel.find().sort({ order: 'asc' });
+  }
+  createUnit(createUnitDto: CreateUnitDto) {
+    return this.unitModel.create(createUnitDto);
+  }
+  updateUnit(id: number, updates: UpdateQuery<Unit>) {
+    return this.unitModel.findByIdAndUpdate(id, updates, {
+      new: true,
+    });
+  }
+  removeUnit(id: number) {
+    return this.unitModel.findByIdAndRemove(id);
+  }
 }
