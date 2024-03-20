f415ecb01aa259a2ad56235001be92215110c043
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-03-19 20:51:04 -0500

    stock type is transfered

--- src/modules/accounting/product.schema.ts
+++ src/modules/accounting/product.schema.ts
@@ -3,6 +3,7 @@ import { Document } from 'mongoose';
 import { purifySchema } from 'src/lib/purifySchema';
 import { Brand } from './brand.schema';
 import { ExpenseType } from './expenseType.schema';
+import { StockType } from './stockType.schema';
 import { Unit } from './unit.schema';
 import { Vendor } from './vendor.schema';
 
@@ -20,10 +21,13 @@ export class Product extends Document {
   @Prop({ required: true, type: [{ type: String, ref: ExpenseType.name }] })
   expenseType: ExpenseType[];
 
-  @Prop({ required: true, type: [{ type: String, ref: Brand.name }] })
+  @Prop({ required: true, type: String, ref: StockType.name })
+  stockType: StockType;
+
+  @Prop({ required: false, type: [{ type: String, ref: Brand.name }] })
   brand: Brand[];
 
-  @Prop({ required: true, type: [{ type: String, ref: Vendor.name }] })
+  @Prop({ required: false, type: [{ type: String, ref: Vendor.name }] })
   vendor: Vendor[];
 
   @Prop({ required: true, type: String, ref: Unit.name })
