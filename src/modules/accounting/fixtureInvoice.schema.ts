9d55a5b29f02f1f9c59093423b9b3b105623c4b1
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-04-25 21:45:21 -0500

    fixture schema updated

--- src/modules/accounting/fixtureInvoice.schema.ts
+++ src/modules/accounting/fixtureInvoice.schema.ts
@@ -12,29 +12,29 @@ export class FixtureInvoice extends Document {
   @Prop({ type: Number })
   _id: number;
 
-  @Prop({ required: true, type: String, ref: Fixture.name })
-  fixture: Fixture;
-
   @Prop({ required: true, type: String, ref: ExpenseType.name })
   expenseType: ExpenseType;
 
+  @Prop({ required: true, type: String, ref: Fixture.name })
+  fixture: Fixture;
+
   @Prop({ required: true, type: Number })
   quantity: number;
+
+  @Prop({ required: false, type: String, ref: Brand.name })
+  brand: Brand;
+
+  @Prop({ required: true, type: Number, ref: Location.name })
+  location: Location;
+
+  @Prop({ required: false, type: String, ref: Vendor.name })
+  vendor: Vendor;
 
   @Prop({ required: true, type: Number })
   totalExpense: number;
 
   @Prop({ required: true })
   date: string;
-
-  @Prop({ required: false, type: String, ref: Brand.name })
-  brand: Brand;
-
-  @Prop({ required: false, type: String, ref: Vendor.name })
-  vendor: Vendor;
-
-  @Prop({ required: true, type: Number, ref: Location.name })
-  location: Location;
 
   @Prop({ required: false })
   note: string;
