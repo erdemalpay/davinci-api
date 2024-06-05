8d11cb8fe3ed3ebe7634ec6ffa071746ca3c28d9
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-06-05 10:06:30 -0500

    expense is removed from checkout

--- src/modules/checkout/checkout.dto.ts
+++ src/modules/checkout/checkout.dto.ts
@@ -4,13 +4,6 @@ export class CreateIncomeDto {
   amount: number;
 }
 
-export class CreateExpenseDto {
-  location: string;
-  date: string;
-  amount: number;
-  description: string;
-}
-
 export class CreateCashoutDto {
   location: string;
   date: string;
--- src/modules/checkout/checkout.module.ts
+++ src/modules/checkout/checkout.module.ts
@@ -4,12 +4,10 @@ import { createAutoIncrementConfig } from 'src/lib/autoIncrement';
 import { Cashout, CashoutSchema } from './cashout.schema';
 import { CheckoutController } from './checkout.controller';
 import { CheckoutService } from './checkout.service';
-import { Expense, ExpenseSchema } from './expense.schema';
 import { Income, IncomeSchema } from './income.schema';
 
 const mongooseModule = MongooseModule.forFeatureAsync([
   createAutoIncrementConfig(Income.name, IncomeSchema),
-  createAutoIncrementConfig(Expense.name, ExpenseSchema),
   createAutoIncrementConfig(Cashout.name, CashoutSchema),
 ]);
 
