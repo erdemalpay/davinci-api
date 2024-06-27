24e7d33cf92d97f53a83bf5b737a560d804f95ba
Erdem Alpay <erdemalpay@gmail.com>
2024-06-27 23:30:48 +0300

    Fix order import error

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,6 @@ import { LocationModule } from './modules/location/location.module';
 import { MembershipModule } from './modules/membership/membership.module';
 import { MenuModule } from './modules/menu/menu.module';
 import { MigrationModule } from './modules/oldDbModules/migration/migration.module';
-import { OrderModule } from './modules/order/order.module';
 import { PanelControlModule } from './modules/panelControl/panelControl.module';
 import { ReservationModule } from './modules/reservation/reservation.module';
 import { RewardModule } from './modules/reward/reward.module';
@@ -48,7 +47,6 @@ const modules = [
   AccountingModule,
   CheckoutModule,
   PanelControlModule,
-  OrderModule,
 ];
 
 if (config.get('migrationEnabled')) {
