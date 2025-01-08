d6d37a4ef60687f24c4e604d45028c9cdecf0c45
Erdem Alpay <erdemalpay@gmail.com>
2025-01-08 10:03:52 +0300

    Fix stock update

--- src/modules/accounting/accounting.controller.ts
+++ src/modules/accounting/accounting.controller.ts
@@ -457,7 +457,7 @@ export class AccountingController {
       payload.currentStockLocation,
       payload.transferredStockLocation,
       payload.product,
-      payload.quantity,
+      Number(payload.quantity),
     );
   }
 
