6f4272bda372064a726ed875354365c03e7ee0cc
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2025-01-22 09:46:09 -0600

    make adjustments at buttonCallSchema

--- src/modules/buttonCall/schemas/buttonCall.schema.ts
+++ src/modules/buttonCall/schemas/buttonCall.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { User } from 'src/modules/oldDbModules/oldUser/user.schema';
+import { User } from 'src/modules/user/user.schema';
 
 @Schema({ _id: false, timestamps: true })
 export class ButtonCall {
