cecf22106f15ecd7d3770fa74b87b5783b157b4f
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2025-01-28 18:01:34 -0600

    trying to fix create collection

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1402,33 +1402,36 @@ export class OrderService {
         .exec();
     }
     const { newOrders, ...filteredCollectionDto } = createCollectionDto;
-    // if (tableCollections?.length > 0) {
-    //   const tablePaidOrders = this.aggregatePaidQuantities(tableCollections);
-    //   if (newOrders && newOrders?.length > 0) {
-    //     filteredCollectionDto.orders.forEach((orderCollectionItem) => {
-    //       const foundNewOrder = newOrders.find(
-    //         (newOrder) => newOrder._id === orderCollectionItem.order,
-    //       );
-    //       const existingOrder: any = tablePaidOrders?.find(
-    //         (paidOrder: any) => paidOrder.order === orderCollectionItem.order,
-    //       );
-    //       if (existingOrder) {
-    //         if (
-    //           existingOrder.paidQuantity + orderCollectionItem.paidQuantity >
-    //             foundNewOrder.quantity ||
-    //           existingOrder?.paidQuantity +
-    //             orderCollectionItem?.paidQuantity !==
-    //             foundNewOrder?.paidQuantity
-    //         ) {
-    //           throw new HttpException(
-    //             `The quantity of order  is exceeded`,
-    //             HttpStatus.BAD_REQUEST,
-    //           );
-    //         }
-    //       }
-    //     });
-    //   }
-    // }
+    if (tableCollections?.length > 0) {
+      const tablePaidOrders = this.aggregatePaidQuantities(tableCollections);
+      if (newOrders && newOrders?.length > 0) {
+        filteredCollectionDto.orders.forEach((orderCollectionItem) => {
+          const foundNewOrder = newOrders.find(
+            (newOrder) => newOrder._id === orderCollectionItem.order,
+          );
+          const existingOrder: any = tablePaidOrders?.find(
+            (paidOrder: any) => paidOrder.order === orderCollectionItem.order,
+          );
+          console.log(existingOrder, 'existingOrder');
+          console.log(foundNewOrder, 'foundNewOrder');
+          console.log(
+            orderCollectionItem.paidQuantity,
+            'orderCollectionItem.paidQuantity',
+          );
+          const expectedPaidQuantity =
+            (existingOrder?.paidQuantity ?? 0) +
+            orderCollectionItem.paidQuantity;
+          console.log(expectedPaidQuantity, 'expectedPaidQuantity');
+
+          if (foundNewOrder.paidQuantity !== expectedPaidQuantity) {
+            throw new HttpException(
+              `The quantity of order  is exceeded`,
+              HttpStatus.BAD_REQUEST,
+            );
+          }
+        });
+      }
+    }
     const collection = new this.collectionModel({
       ...filteredCollectionDto, // Use the filtered object
       createdBy: createCollectionDto.createdBy ?? user._id,
