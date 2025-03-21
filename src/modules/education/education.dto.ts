aeed21358450c2df68485713a700fdd40dfe8af1
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2025-03-20 21:17:27 -0500

    education schema and dto is adjusted

--- src/modules/education/education.dto.ts
+++ src/modules/education/education.dto.ts
@@ -1,3 +1,26 @@
-export class EducationDto {
-  // ...add properties as needed...
+export class StyleDto {
+  color?: string;
+  backgroundColor?: string;
+  fontSize?: string;
+  fontWeight?: string;
+  textAlign?: string;
+  imageHeight?: string;
+  imageWidth?: string;
+  imageBorderRadius?: string;
+  imageMargin?: string;
+}
+
+export class EducationSubheaderDto {
+  componentType: string;
+  subHeader?: string;
+  paragraph?: string;
+  imageUrl?: string;
+  style?: StyleDto;
+}
+
+export class CreateEducationDto {
+  permissionRoles: number[];
+  header: string;
+  order: number;
+  subheaders?: EducationSubheaderDto[];
 }
--- src/modules/education/education.schema.ts
+++ src/modules/education/education.schema.ts
@@ -1,9 +1,66 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+class Style {
+  @Prop({ required: false, type: String })
+  color: string;
+
+  @Prop({ required: false, type: String })
+  backgroundColor: string;
+
+  @Prop({ required: false, type: String })
+  fontSize: string;
+
+  @Prop({ required: false, type: String })
+  fontWeight: string;
+
+  @Prop({ required: false, type: String })
+  textAlign: string;
+
+  @Prop({ required: false, type: String })
+  imageHeight: string;
+
+  @Prop({ required: false, type: String })
+  imageWidth: string;
+
+  @Prop({ required: false, type: String })
+  imageBorderRadius: string;
+
+  @Prop({ required: false, type: String })
+  imageMargin: string;
+}
+class Subheader {
+  @Prop({ required: true, type: String })
+  componentType: string;
+
+  @Prop({ required: false, type: String })
+  subHeader: string;
+
+  @Prop({ required: false, type: String })
+  paragraph: string;
+
+  @Prop({ required: false, type: String })
+  imageUrl: string;
+
+  @Prop({ required: false, type: Style })
+  style: Style;
+}
 @Schema({ _id: false })
 export class Education extends Document {
-  // ...empty schema...
+  @Prop({ type: Number })
+  _id: number;
+
+  @Prop({ required: true, type: [Number] })
+  permissionRoles: number[];
+
+  @Prop({ required: true, type: String })
+  header: string;
+
+  @Prop({ required: true, type: Number })
+  order: number;
+
+  @Prop([Subheader])
+  subheaders: Subheader[];
 }
 
 export const EducationSchema = SchemaFactory.createForClass(Education);
