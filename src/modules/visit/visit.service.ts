33cfa80d4914e4c9307d296d725e2740af979c71
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2025-05-26 08:50:39 -0500

    fix shift message

--- src/modules/visit/visit.service.ts
+++ src/modules/visit/visit.service.ts
@@ -203,7 +203,7 @@ export class VisitService {
             selectedRoles: [1],
             seenBy: [],
             event: '',
-            message: `${user.name} are late for shift. Shift was at ${foundShift.shift} and ${user.name} entered at ${cafeVisitDto.hour}`,
+            message: `${user.name} is late for shift. Shift was at ${foundShift.shift} and ${user.name} entered at ${cafeVisitDto.hour}`,
           });
         }
       }
