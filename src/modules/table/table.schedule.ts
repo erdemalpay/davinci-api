98b5c06ac04118fd1e49f83cdcdbf9d489691058
Erdem Alpay <erdemalpay@gmail.com>
2025-06-25 08:06:03 +0300

    Test daily schedule

--- src/modules/table/table.schedule.ts
+++ src/modules/table/table.schedule.ts
@@ -1,15 +1,29 @@
 // table.schedule.ts
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { TableService } from './table.service';
 
 @Injectable()
 export class TableSchedule {
+  private readonly logger: Logger = new Logger('TableSchedule');
+
   constructor(private readonly tableService: TableService) {}
 
   @Cron('0 0 1 * * *', { timeZone: 'Europe/Istanbul' })
+  async handleEveryDayCheck() {
+    this.logger.log('🔄 [Cron] Checking for unclosed tables…');
+    await this.tableService.notifyUnclosedTables();
+  }
+  
+  @Cron('0 * * * * *')
   async handleEveryMinuteCheck() {
-    console.log('🔄 [Cron] Checking for unclosed tables…');
+    this.logger.log('🔄 [Cron] Checking every minute', new Date().toISOString());
+    await this.tableService.notifyUnclosedTables();
+  }
+  
+  @Cron('10 * * * * *', { timeZone: 'Europe/Istanbul' })
+  async handleEveryMinuteWithTimezoneCheck() {
+    this.logger.log('🔄 [Cron] Checking every minute with timezone', new Date().toISOString());
     await this.tableService.notifyUnclosedTables();
   }
 }
