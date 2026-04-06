9a711e2997ed6ec951badb1c9d84d0d6287acab1
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2026-04-06 16:16:01 -0500

    local comparision page is added

---
+++ src/modules/price-compare/price-compare.module.ts
@@ -0,0 +1,23 @@
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MenuModule } from '../menu/menu.module';
+import {
+  LocalComparison,
+  LocalComparisonSchema,
+} from './local-comparison.schema';
+import { PriceCompareController } from './price-compare.controller';
+import { PriceCompareCronService } from './price-compare.cron.service';
+import { PriceCompareService } from './price-compare.service';
+
+const mongooseModule = MongooseModule.forFeatureAsync([
+  { name: LocalComparison.name, useFactory: () => LocalComparisonSchema },
+]);
+
+@Module({
+  imports: [HttpModule, MenuModule, mongooseModule],
+  controllers: [PriceCompareController],
+  providers: [PriceCompareService, PriceCompareCronService],
+  exports: [PriceCompareService],
+})
+export class PriceCompareModule {}
