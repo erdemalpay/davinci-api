864c7a35c75d4d13c1354fbd7127d9d1e7741a5f
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2026-04-21 10:07:34 -0500

    retailer order logic changed

--- src/modules/order/retailer.schema.ts
+++ src/modules/order/retailer.schema.ts
@@ -1,7 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { purifySchema } from 'src/lib/purifySchema';
-import { Order } from './order.schema';
 
 @Schema({ _id: false })
 export class Retailer extends Document {
@@ -10,9 +9,6 @@ export class Retailer extends Document {
 
   @Prop({ required: true, type: String })
   name: string;
-
-  @Prop({ required: false, type: [Number], ref: Order.name, default: [] })
-  orders: number[];
 }
 
 export const RetailerSchema = SchemaFactory.createForClass(Retailer);
